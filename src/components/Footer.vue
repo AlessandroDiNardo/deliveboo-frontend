23817fe22faac9b9a5f26177d660c6da7b0ec6ca
Yari <narducciyarigmail.com>
2023-03-01 19:14:13 +0100

    Modifiche footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,59 @@
 <script>
 
+export default{
+    data() {
+        return {
+            items:[{'title':'Scopri Deliveboo',
+                    links:[ 'Investitori',
+                    'Chi siamo',
+
+                    'Ristoranti',
+
+                    'Altro',
+
+                   ' Pressroom',
+
+                   ' Programmazione',
+
+                   ' Design',
+
+                    'Lavora con noi',
+
+                   ' Diventa nostro partner']},
+
+                   {'title':'Note legali',
+                   
+                    links:[    ' Termini & Condizioni',
+
+                               ' Informativa sulla privacy',
+
+                                'Cookies']},
+
+                   {'title':'Aiuto',
+                   
+                   links:[    ' Termini & Condizioni',
+
+                              ' Informativa sulla privacy',
+
+                               'Cookies']},
+
+                  {'title':'Porta Deliveroo con te',
+                   
+                  links:[    ' Termini & Condizioni',
+
+                                ' Informativa sulla privacy',
+
+                                ]},
+
+                
+                ]
+        }
+    },
+
+                   
+            
+                
+        }
 
 
 
@@ -8,75 +62,72 @@
 
 <template>
 
-    <footer class="py-3 my-4 text-white" >
+    <footer class="py-3 my-4 " >
        
-        <h2  data-aos="flip-up"
+        <h2  class="mb-2" data-aos="flip-up"
         data-aos-easing="ease-out-cubic"
         data-aos-duration="1200">
           I Nostri Contatti
         </h2>
-        <div class="list_container w-75 m-auto">
-          <ul>
-              <li>Titolo</li>
-              <li>link</li>
-              <li>link</li>
-              <li>link</li>
-              <li>link</li>
-              <li>link</li>
+        <div class="list_container w-75 m-auto d-flex " >
+          <ul v-for="(element, index) in items" class="rounded-2">
+              <li><h6 class="py-2">{{element.title}}</h6></li>
+              <li v-for="link in element.links " class="pb-2" id="footer_ul">
+               <a href="#"  >
+                {{ link }}
+               </a>
+              </li>
           </ul>
-          <ul>
-              <li>Titolo</li>
-              <li>link</li>
-              <li>link</li>
-              <li>link</li>
-              <li>link</li>
-              <li>link</li>
-          </ul>
-          <ul>
-              <li>Titolo</li>
-              <li>link</li>
-              <li>link</li>
-              <li>link</li>
-              <li>link</li>
-              <li>link</li>
-          </ul>
-          <ul>
-              <li>Titolo</li>
-              <li>link</li>
-              <li>link</li>
-              <li>link</li>
-              <li>link</li>
-              <li>link</li>
-          </ul>
+      
         </div>
       </footer>
 
 </template>
 
-<style >
+<style lang="scss" >
 footer{
     background-color: #2e3333;
+    color: white;
    
 }
+h2{
+    color: #069e24;
+
+}
     ul{
-        width: 150px;
-        margin: auto 20px;
+       
+        margin: auto 15px;
         list-style: none;
         background-color: #434848;
-        padding: 10px;
+        padding: 0 10px;
         display: flex;
         flex-direction: column;
-        justify-content: center;
-        align-items: center;
+
+       
         
 
     }
     ul li{
-        text-align: center;
+        text-align:start;
       
     }
+    #footer_ul{
+        padding-left: 0%;
+    }
+    li a{
+        color:  #069e24;
+        transition: all 0.2s;
+        text-decoration: none;
+        &:hover{
+            text-decoration: underline;
+            color: white;
+
+        }
+    }
     .list_container{
+        margin-top: 20px;
         display: flex;
         justify-content: center;
+        flex-wrap: wrap;
     }
 </style>
