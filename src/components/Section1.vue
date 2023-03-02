27c0b4376baf0e676556da0a238cf0f7a5c78346
Yari <narducciyarigmail.com>
2023-03-02 17:38:54 +0100

    Modifica Sezione Work with us

--- src/components/Section1.vue
+++ src/components/Section1.vue
@@ -8,59 +8,94 @@
 
 <template>
     <section class="work_us">
-        <div class="container-fluid  row justify-content-center py-5 col-11 m-auto " id="Wrapper_Container">
-            <div  data-aos="fade-down"
-            data-aos-easing="linear"
-            data-aos-duration="1000" class="col-5  mx-1 " id="txt_cont">
-            <h2>LAVORA CON NOI</h2>
-            <p class="txt_p">
-                Sei un morto di fame? <br>
-                Mangi pane e merda a colazione,pranzo e cena? <br>
-                Hai delle aspettative economiche talmente basse che al confronto gli schiavi dei sudisti avevano un ottima apettativa? <br>
-                Allora Deliveboo è il regime di schiavitù che fa per te!!!!! <br>
-                Paga da fame , orari di merda ,trattamento come lo sporco delle unghie....... <br>
-                E questi sono solo alcuni dei benefit che la nostra azienda offre... <br>
-                Che aspetti? <a class="bg-primary text-white rounded-1 text-decoration-none mx-1 p-1" href="#">Lavora con Noi</a>
-            </p>
+        <div class="container-fluid">
+           
+         </div>
+         <div class="container-fluid  row justify-content-center py-5 col-11 m-auto " id="Wrapper_Container">
+             <div class="row">
+                
+                
+                   <div class="col-md-4">
+                     <div class="card mb-4  " style="background-color:#ff8100;border:none;color:white;">
+                       <img class="card-img-top" alt="Thumbnail [100%x225]" style="  width: 100%; display: block;  clip-path: polygon(0 0, 100% 0, 100% 68%, 0 90%); " src="rider.jpg">
+                       <div class="card-body p-4">
+                        <h1 >
+                          Ristoranti
+                        </h1>
+                         <p>Diventa partner di Deliveroo e raggiungi sempre più clienti. Ci occupiamo noi della consegna, così che la tua unica preoccupazione sia continuare a preparare il miglior cibo.</p>
+                         <div class="text-center">
+                            <button class="bg-primary border-0 rounded-2 p-2">
+                                <a href="" class="text-decoration-none text-white">Scopri di più</a>
+                            </button>
+                           </div>
+                        </div>
+                      
+                     </div>
+                   </div>
+
+                   <div class="col-md-4">
+                    <div class="card mb-4  " style="background-color:#069e24;border:none;color:white;">
+                      <img class="card-img-top" alt="Thumbnail [100%x225]" style=" width: 100%; display: block;  clip-path: polygon(0 0, 100% 0, 100% 68%, 0 90%); " src="Ristoranti.jpg">
+                      <div class="card-body p-4">
+                       <h1 >
+                        Lavora con noi
+                       </h1>
+                        <p>La nostra missione è trasformare il modo in cui le persone mangiano. È un obiettivo ambizioso, come noi, e ci servono persone che ci aiutino a raggiungerlo. </p>
+                        <div class="text-center">
+                           <button class="bg-primary border-0 rounded-2 p-2 ">
+                               <a href="" class="text-decoration-none text-white">Scopri di più</a>
+                           </button>
+                          </div>
+                       </div>
+                     
+                    </div>
+                  </div>
+
+                  <div class="col-md-4">
+                    <div class="card mb-4  " style="background-color:#ff483b;border:none;color:white;">
+                      <img class="card-img-top" alt="Thumbnail [100%x225]" style=" width: 100%; display: block;  clip-path: polygon(0 0, 100% 0, 100% 68%, 0 90%); " src="Lavora.jpg">
+                      <div class="card-body p-4">
+                       <h1 >
+                         Riders
+                       </h1>
+                        <p>Diventa un rider: flessibilità, ottimi guadagni e un mondo di vantaggi per te.</p>
+                        <div class="text-center">
+                           <button class="bg-primary border-0 rounded-2 p-2 mt-5">
+                               <a href="" class="text-decoration-none text-white">Scopri di più</a>
+                           </button>
+                          </div>
+                       </div>
+                     
+                    </div>
+                  </div>
+                   
+     
+             </div>
         </div>
-        <div id="cont_img" data-aos="fade-right"
-        data-aos-easing="linear"
-        data-aos-duration="1000" class="col-5 rounded mx-1" >
-            <img  id="img_cont" src="../assets/lavora-con-noi.jpg" alt="">
-        </div>
-       </div>
       </section>
 </template>
 
 <style lang="scss">
     .work_us{
-        background-color: #069e24;
+        background-color: white;
     }
+.clip{
+    clip-path: polygon(0 0, 100% 24%, 100% 100%, 0% 100%);
+    border-radius: 12%;
+} 
 
-   #img_cont{
-    width: 100%;
-    border-radius: 15px;
-   }
-   #txt_cont{
-    
-    color: white;
-   }
-   h2{
+.card_text{
+   
     text-align: center;
-   }
-   .txt_p{
-    font-size: 15px;
-   }
-   @media (max-width: 992px) and (min-width: 350px) { 
+}
+.Circle_round{
+    border-radius: 25px;
+    border: 1px solid white;
+    
+}
+.title_color{
+    color: white;
+}
 
-    #Wrapper_Container{
-        display: flex;
-        flex-direction: column;
-        align-content: center;
-        }
-       #cont_img,#txt_cont{
-        width: 100%;
-        color: white;
-       } 
-    }
+ 
 </style>
